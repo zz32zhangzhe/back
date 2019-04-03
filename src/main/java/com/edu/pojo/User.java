400a4008ed0946141aa4f3c97b3c80ae51aa6c8e
zz32zhangzhe <49133244+zz32zhangzhe@users.noreply.github.com>
2019-04-03 10:27:03 +0800

    三次添加 user表 dao 类 dao mapper

---
+++ src/main/java/com/edu/pojo/User.java
@@ -0,0 +1,105 @@
+package com.edu.pojo;
+
+import java.util.Date;
+
+public class User {
+    private Integer id;
+
+    private String loginId;
+
+    private String name;
+
+    private String password;
+
+    private Date createDate;
+
+    private Date lastDate;
+
+    private String phone;
+
+    private String email;
+
+    private Integer sex;
+
+    private Integer status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId == null ? null : loginId.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getLastDate() {
+        return lastDate;
+    }
+
+    public void setLastDate(Date lastDate) {
+        this.lastDate = lastDate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
